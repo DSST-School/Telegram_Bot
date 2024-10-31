26075db663803d741f5b73999f641b6d2132055b
Yana1124 <nikt3571@gmail.com>
2024-10-31 10:07:40 +0200

    Update BotYana.py

--- BotYana.py
+++ BotYana.py
@@ -1,17 +1,22 @@
 import telebot
 
 # Замените 'YOUR_BOT_TOKEN' на ваш токен
-bot = telebot.TeleBot('YOUR_BOT_TOKEN')
+API_TOKEN = 'YOUR_BOT_TOKEN'
+bot = telebot.TeleBot(API_TOKEN)
+
+# Приветственное сообщение
+WELCOME_MESSAGE = "Привет! Я Telegram-бот. Чем могу помочь?"
+HELP_MESSAGE = "Я могу ответить на ваши сообщения. Просто напишите мне что-нибудь!"
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я Telegram-бот. Чем могу помочь?")
+    bot.reply_to(message, WELCOME_MESSAGE)
 
 # Обработчик команды /help
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message, "Я могу ответить на ваши сообщения. Просто напишите мне что-нибудь!")
+    bot.reply_to(message, HELP_MESSAGE)
 
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
@@ -19,4 +24,5 @@ def echo_all(message):
     bot.reply_to(message, f"Вы написали: {message.text}")
 
 # Запуск бота
-bot.polling(none_stop=True)
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
